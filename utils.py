a3b291a7b11cfb069b7931d0abaea5701c489fa5
Sheshansh Agrawal <sheshansh@cse.iitb.ac.in>
2018-11-26 21:27:51 +0530

    Adding data directory argument

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
 
-def create_if_not_exists():
-    if not os.path.exists("tb_logs"):
-        os.mkdir("tb_logs")
-    if not os.path.exists("saved_models"):
-        os.mkdir("saved_models")
+def create_if_not_exists(args):
+    model_dir = os.path.join(args.data_dir, 'saved_models')
+    log_dir = os.path.join(args.data_dir, 'tb_logs')
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
 
 # import numpy as np
 # for file in os.listdir('EyeInTheSky/data/gt/'):
